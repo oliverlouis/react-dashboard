02e7feebf1153ed320678fcc48c0d01c08836fe9
Oliver Louis <oliverlouis@me.com>
2022-05-29 22:49:49 +0200

    Add revenue card, Sparkline Chart and Stacked Chart to Ecommerce page

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
-const SparlLine = () => {
-  return (
-    <div>SparlLine</div>
-  )
+class SparkLine extends React.PureComponent {
+	render() {
+		const { id, height, width, color, data, type, currentColor } = this.props;
+
+		return (
+			<SparklineComponent
+				id={id}
+				height={height}
+				width={width}
+				lineWidth={1}
+				valueType="Numeric"
+				fill={color}
+				border={{ color: currentColor, width: 2 }}
+				tooltipSettings={{
+					visible: true,
+					// eslint-disable-next-line no-template-curly-in-string
+					format: '${x} : data ${yval}',
+					trackLineSettings: {
+						visible: true
+					}
+				}}
+				dataSource={data}
+				xName="x"
+				yName="yval"
+				type={type}>
+				<Inject services={[SparklineTooltip]} />
+			</SparklineComponent>
+		);
+	}
 }
 
-export default SparlLine
+export default SparkLine;
